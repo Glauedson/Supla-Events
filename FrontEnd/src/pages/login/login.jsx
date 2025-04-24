42f343e3e1462abd9f1f1044dd91b4fd5192102e
Glauedson <glauedson18s@gmail.com>
2025-04-23 22:27:30 -0300

    feat: Finalização da pagina de login

--- FrontEnd/src/pages/login/login.jsx
+++ FrontEnd/src/pages/login/login.jsx
@@ -1,8 +1,12 @@
-import { useState } from 'react';
+import { useState } from 'react'
+import styles from './login.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faUnlock } from '@fortawesome/free-solid-svg-icons'
+import { Link } from "react-router-dom"
 
 function Login() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
 
   const handleLogin = async () => {
     if (email === '' || password === '') {
@@ -20,11 +24,11 @@ function Login() {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(dataLogin),
-      });
+      })
 
       if (reply.status !== 200) {
         alert(`Algo errado não está certo: ${reply.status}`);
-        return;
+        return
       }
 
       const data = await reply.json();
@@ -39,49 +43,70 @@ function Login() {
       console.error('Erro ao fazer login:', error);
       alert('Erro ao tentar fazer login');
     }
-  };
+  }
 
   return (
-    <div className="d-flex flex-column justify-content-center align-items-center vh-100">
-      <img className="mb-5" src="../imgs/logo.png" alt="Logo" />
-
-      <form>
-        <div className="mb-3">
-          <label htmlFor="inputEmail" className="form-label">
-            Email:
-          </label>
+    <>
+    <main>
+      <form className={styles.form_container}>
+        <div className={styles.logo_container}></div>
+        <div className={styles.title_container}>
+          <p className={styles.title}>Login</p>
+          <span className={styles.subtitle}>
+            hey <strong>papito</strong>, vamos logar na sua conta?
+          </span>
+        </div>
+        <br />
+        <div className={styles.input_container}>
+          <label className={styles.input_label} htmlFor="email_field">Email</label>
+          <FontAwesomeIcon icon={faEnvelope} className={styles.icon} />
           <input
-            type="email"
-            className="form-control"
-            id="inputEmail"
-            aria-describedby="emailHelp"
+            placeholder="nome@mail.com"
+            title="Coloque seu email"
+            name="input-name"
+            type="text"
+            className={styles.input_field}
+            id="email_field"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
           />
         </div>
-        <div className="mb-3">
-          <label htmlFor="inputPassword" className="form-label">
-            Password
-          </label>
+        <div className={styles.input_container}>
+          <label className={styles.input_label} htmlFor="password_field">Senha</label>
+          <FontAwesomeIcon icon={faUnlock} className={styles.icon} />
           <input
+            placeholder="*********"
+            title="Coloque sua senha"
+            name="input-name"
             type="password"
-            className="form-control"
-            id="inputPassword"
+            className={styles.input_field}
+            id="password_field"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
         </div>
-
-        <button
-          type="button"
-          className="btn btn-primary"
-          onClick={handleLogin}
-        >
-          Login
+        
+        <button className={styles.sign_in_button} onClick={handleLogin} type="button">
+          <span>Entrar</span>
         </button>
+
+        <div className={styles.separator}>
+          <hr className={styles.line} />
+          <span>é novo por aqui papito?</span>
+          <hr className={styles.line} />
+        </div>
+  
+        <button title="Se cadastrar" type="submit" className={styles.sign_up_button}>
+          <Link to="/accounts/register">Cadastre-se</Link>
+        </button>
+
+        <div className={styles.terms}> <Link to="/accounts/terms">termos de uso e condição</Link></div>
+
       </form>
-    </div>
-  )
+
+      </main>
+    </>
+  );
 }
 
 export default Login
